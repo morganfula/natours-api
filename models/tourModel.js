b506ddb09c52ea0895b9f811bcbd86a73c66a962
morganfula <morgan.fula@me.com>
2020-12-24 16:09:07 +0100

    validators.js  added

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+//! const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, 'A tour name must have less or equal then 40 character!'],
       minlength: [10, 'A tour name must have more or equal then 10 character!'],
+      // validate: [validator.isAlpha, 'Tour name must only contain characters'],
     },
     slug: String,
     duration: {
